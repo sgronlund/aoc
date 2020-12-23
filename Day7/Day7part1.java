7caf68feb211eb20c242f9d167dfffb361ee7ee1
sgronlund <sebastian.gronlund.1117@student.uu.se>
2020-12-24 00:50:59 +0200

    Solution for part1 of day7 added

--- Day7/Day7part1.java
+++ Day7/Day7part1.java
@@ -3,32 +3,38 @@ import java.io.*;
 
 public class Day7part1 {
 
+
+    public static boolean checkBagInBag(ArrayList<String> goldBags, String input) {
+        for (String goldBag: goldBags) {
+            if(input.contains(goldBag)) return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         long counter = 0;
         ArrayList<String> goldBags = new ArrayList<String>();
-        ArrayList<String> bagsWithGoldBags = new ArrayList<String>();
-        Scanner scan = new Scanner(new File("day7.txt"));
-        while(scan.hasNextLine()) {
-            String read = scan.nextLine();
-            if (!(read.contains("no other"))) {
-                String[] words = read.split(" ");
-                String currentCol = words[0] + " " + words[1];
-                if (read.contains("shiny gold") && !(currentCol.equals("shiny gold"))) { 
-                        goldBags.add(currentCol);
-                }
-            } else {
-                continue;
-            }
-        }
-        scan.reset();
-        for (String gold: goldBags) {
+        while (true) {
+            long old = counter;
+            Scanner scan = new Scanner(new File("day7.txt"));
             while(scan.hasNextLine()) {
                 String read = scan.nextLine();
-                if (read.contains(gold)) {
-                    bagsWithGoldBags.add(read);
-                    ++counter;
+                if (!(read.contains("no other"))) {
+                    String[] words = read.split(" ");
+                    String currentCol = words[0] + " " + words[1];
+                    if (read.contains("shiny gold") && !(currentCol.equals("shiny gold")) || checkBagInBag(goldBags, read)) { 
+                            if(!goldBags.contains(currentCol)) {
+                                counter++;
+                                goldBags.add(currentCol);
+                            } else {
+                                goldBags.add(currentCol);
+                            }
+                    }
+                } else {
+                    continue;
                 }
             }
+            if (old == counter) break;
         }
         System.out.println("Size var " + counter);
     }
